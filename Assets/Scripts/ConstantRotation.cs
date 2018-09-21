40b16f7e5610ae5bfce16a82edabd1b6d2cc2417
Johannes Deml <send@johannesdeml.com>
2018-09-21 17:00:11 +0200

    Add project

---
+++ Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,114 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConstantRotation.cs" company="Supyrb">
+//   Copyright (c) 2017 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   send@johannesdeml.com
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+
+namespace Supyrb
+{
+	public class ConstantRotation : MonoBehaviour
+	{
+		public enum UpdateType
+		{
+			DeltaTime,
+			SmoothDeltaTime,
+			UnscaledDeltaTime,
+			Constant
+		}
+
+		[Tooltip("Rotating absolute in world space or relative to the axes of the object\n" +
+				"For root objects use self, same effect but more performant.")]
+		[SerializeField]
+		private Space space = Space.World;
+
+		[SerializeField]
+		private Vector3 rotationAxis = Vector3.up;
+
+		[SerializeField]
+		private float degreePerSecond = 90f;
+
+		[SerializeField]
+		private UpdateType updateType = UpdateType.DeltaTime;
+
+		private Quaternion rotationPerUpdate;
+
+		private float deltaTime
+		{
+			get
+			{
+				switch (updateType)
+				{
+					case UpdateType.DeltaTime:
+						return Time.deltaTime;
+					case UpdateType.SmoothDeltaTime:
+						return Time.smoothDeltaTime;
+					case UpdateType.UnscaledDeltaTime:
+						return Time.unscaledDeltaTime;
+					case UpdateType.Constant:
+						return 0.01666f;
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+			}
+		}
+
+
+		void LateUpdate()
+		{
+			CalculatePerFrameRotation();
+			ApplyRotation();
+		}
+
+		private void ApplyRotation()
+		{
+			if (space == Space.World)
+			{
+				transform.rotation = rotationPerUpdate * transform.rotation;
+			}
+			else
+			{
+				transform.localRotation *= rotationPerUpdate;
+			}
+		}
+
+		public void CalculatePerFrameRotation()
+		{
+			rotationPerUpdate = Quaternion.AngleAxis(degreePerSecond * deltaTime, rotationAxis);
+		}
+
+		public void SetRotationPerSecond(float rotationPerSecond)
+		{
+			degreePerSecond = rotationPerSecond;
+		}
+
+		public void SetXRotationAxis(float x)
+		{
+			rotationAxis.x = x;
+		}
+
+		public void SetYRotationAxis(float y)
+		{
+			rotationAxis.y = y;
+		}
+
+		public void SetZRotationAxis(float z)
+		{
+			rotationAxis.z = z;
+		}
+
+		#if UNITY_EDITOR
+		void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawRay(transform.position, (space == Space.Self) ? transform.rotation * rotationAxis.normalized : rotationAxis.normalized);
+		}
+		#endif
+	}
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectSpawner.cs" company="Supyrb">
+//   Copyright (c) 2018 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   send@johannesdeml.com
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject prefab = null;
+
+	[SerializeField]
+	private int spawnCooldownFrames = 15;
+
+	[SerializeField]
+	private int maxInstances = 200;
+	
+	private int counter = 0;
+	private int instances = 0;
+	private Queue<GameObject> spawnedObjects = null;
+
+	void Awake()
+	{
+		spawnedObjects = new Queue<GameObject>(maxInstances + 5);
+	}
+	
+	void Update()
+	{
+		counter++;
+		if (counter >= spawnCooldownFrames)
+		{
+			SpawnObject();
+			counter = 0;
+		}
+	}
+
+	private void SpawnObject()
+	{
+		if (instances >= maxInstances)
+		{
+			var recycleGo = spawnedObjects.Dequeue();
+			recycleGo.transform.localPosition = transform.position;
+			recycleGo.transform.localRotation = transform.localRotation;
+			spawnedObjects.Enqueue(recycleGo);
+			return;
+		}
+		
+		var newGo = Instantiate(prefab, transform.position, transform.rotation);
+		spawnedObjects.Enqueue(newGo);
+		instances++;
+	}
+	
+	#if UNITY_EDITOR
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere(transform.position, 0.5f);
+	}
+	#endif
+}
