6cef2593edc4d50cefbde03ef2776eecc052c08d
Johannes Deml <public@deml.io>
2025-12-15 23:41:50 +0100

    Add example for changing the cursor
    Since this now shows up as a pointer, I added a Destroy on click logic, makes it a bit more fun :)

---
+++ Assets/Scripts/ChangeCursorExample.cs
@@ -0,0 +1,35 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ChangeCursorExample.cs">
+//   Copyright (c) 2025 Johannes Deml. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Supyrb
+{
+	public class ChangeCursorExample : MonoBehaviour
+	{
+		public string CursorEnteredName = "pointer";
+		public string CursorExitedName = "default";
+
+		private void OnMouseEnter()
+		{
+			WebToolPlugins.SetCursor(CursorEnteredName);
+		}
+
+		private void OnMouseExit()
+		{
+			WebToolPlugins.SetCursor(CursorExitedName);
+		}
+
+		private void OnMouseDown()
+		{
+			Destroy(gameObject);
+		}
+	}
+}
