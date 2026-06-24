0cbcfa90ced37d42a0006d019950e07a805f241c
Johannes Deml <public@deml.io>
2026-06-24 23:18:19 +0200

    remove astc enforcing

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -132,8 +132,6 @@ namespace UnityBuilderAction
 							// Off by default, but just to be sure, see https://docs.unity3d.com/Manual/webgl-embeddedresources.html
 							PlayerSettings.WebGL.useEmbeddedResources = false;
 							PlayerSettings.WebGL.threadsSupport = false;
-							// ASTC has the smallest texture sizes
-							EditorUserBuildSettings.webGLBuildSubtarget = WebGLTextureSubtarget.ASTC;
 
 #if UNITY_2022_1_OR_NEWER
 							PlayerSettings.SetIl2CppCodeGeneration(namedBuildTarget, Il2CppCodeGeneration.OptimizeSize);
